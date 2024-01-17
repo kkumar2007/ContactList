de8b3e8d1bb764e549dde29954715b77c76b3d88
Kavan <kavan.kumar@menloschool.org>
2024-01-16 21:58:05 -0800

    I created the Person Student and Athlete class

--- src/ContactList.java
+++ src/ContactList.java
@@ -14,6 +14,18 @@ public class ContactList
     public void addContact()
     {
         Scanner s = new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("1. Add Contact");
+        System.out.println("2. List All Contacts by First Name");
+        System.out.println("3. List All Contacts by Last Name");
+        System.out.println("4. List All Contacts by Phone Number");
+        System.out.println("5. List All Students");
+        System.out.println("6. Search by First Name");
+        System.out.println("7. Search by Last Name");
+        System.out.println("8. Search by Phone Number");
+        System.out.println("0. Exit");
+        int menu = s.nextInt();
+        s.nextLine();
 
     }
 }
