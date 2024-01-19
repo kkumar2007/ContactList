48563d9bbd4b7cdf490f0dc3940f7be1b95f44f2
Kavan <kavan.kumar@menloschool.org>
2024-01-19 13:34:31 -0800

    I finished all the classes and added a main method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        Person p = new Person("Kavan", "Kumar", "9294180042");
-        Student s = new Student("Kairav", "Kumar", "2039979276", 7);
-        ContactList contact = new ContactList();
-        contact.run();
     }
 }
